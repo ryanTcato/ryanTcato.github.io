af2c16aaf039a93b64633ff4c85556d07d17664d
ryanTcato <ryantcato@gmail.com>
2022-11-15 17:50:10 +0000

    circ progress

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circle;
+var circles= [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+ physikz.addRandomVelocity(circle, canvas);
+ view.addChild(circle);
+ circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+drawCircle();
+drawCircle();
+drawCircle();
+drawCircle();
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,7 +47,7 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+physikz.updatePosition();
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
